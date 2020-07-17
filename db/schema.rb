b80e9f0f79d050952c6c3055f0b23d6c24d455de
Sina Rostam <sinarostam@gmail.com>
2020-07-16 17:21:53 -0700

    Add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_190059) do
+ActiveRecord::Schema.define(version: 2020_07_16_171106) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_07_13_190059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chef_id"
+    t.string "picture"
   end
 
 end
