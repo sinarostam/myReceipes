5fafa5d510f5a8f1f569c0e175de3b27190ddaec
Sina Rostam <sinarostam@gmail.com>
2020-07-20 10:35:51 -0700

    add requirements and validations before submiting data

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,7 @@
 class ChefsController < ApplicationController
+	before_action :set_chef, only: [:edit, :update, :show] #repeat was removed and set in to the set_chef method in private
+	before_action :require_same_user, only: [:edit, :update]#this will get exectued first action will get done first
+
 	def index
 		@chefs = Chef.paginate(page: params[:page], per_page: 3)
 	end
@@ -20,25 +23,33 @@ class ChefsController < ApplicationController
 
 
 	def edit
-		@chef = Chef.find(params[:id])
 	end
 
 	def update
-		@chef = Chef.find(params[:id])
 		if @chef.update(chef_params)
 			flash[:success] = "Your Profile Has Been Updated Successfully"
-			redirect_to recipes_path  #todo : change to show chef page
+			redirect_to chef_path(@chef)
 		else
 			render 'edit'
 		end
 	end
 
 	def show
-		@chef = Chef.find(params[:id])
 		@recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
 	end
 	private
 		def chef_params
 			params.require(:chef).permit(:chefname, :email, :password)
+		end
+		
+		def set_chef
+			@chef = Chef.find(params[:id])
+		end
+
+		def require_same_user
+			if !!current_user != @chef_id
+				flash[:danger] = "You can only edit your won Profile"
+				redirect_to root_path
+			end
 		end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,14 @@
 class RecipesController < ApplicationController
+	before_action :set_recipe, only: [:edit, :update, :show, :like]  #		@recipe = Recipe.find(params[:id])
+	before_action :require_user, except: [:show, :index] #user does not have to be logged in to see the recipes
+	before_action :require_same_user, only: [:edit, :update]
+	#order is very important
+
 	def index
 		@recipes = Recipe.paginate(page: params[:page], per_page: 4)			#stores the model into a instance variable where it is pushed to controller and availble in the view
 	end
 
 	def show
-		@recipe = Recipe.find(params[:id])
 	end
 
 	def new
@@ -12,7 +16,7 @@ class RecipesController < ApplicationController
 	end
 	def create
 		@recipe = Recipe.new(recipe_params)
-		@recipe.chef= Chef.find(2)
+		@recipe.chef= current_user
 
 		if @recipe.save
 			flash[:success] = "Your Recipe was created successfully"
@@ -23,11 +27,9 @@ class RecipesController < ApplicationController
 	end
 
 	def edit
-		@recipe = Recipe.find(params[:id])
 	end
 
 	def update
-		@recipe = Recipe.find(params[:id])
 		if @recipe.update(recipe_params)
 			flash[:success] = "Your Recipe was updated successfully"
 			redirect_to recipe_path(@recipe)			#redirect to show recipe page after update
@@ -37,8 +39,7 @@ class RecipesController < ApplicationController
 	end
 
 	def like
-		@recipe = Recipe.find(params[:id])
-		like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+		like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
 		if like.valid?
 			flash[:success] = "Your Selection was successful"
 			redirect_back fallback_location: root_path
@@ -52,5 +53,16 @@ class RecipesController < ApplicationController
 	private
 		def recipe_params
 			params.require(:recipe).permit(:name, :summary, :description, :picture)
+		end
+
+		def set_recipe
+			@recipe = Recipe.find(params[:id])
+
+		end
+		def require_same_user
+			if current_user != @recipe.chef
+				flash[:danger] = "You can only edit your own recipes"
+				redirect_to recipes_path
+			end
 		end
 end	
